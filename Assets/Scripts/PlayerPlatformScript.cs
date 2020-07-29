4e9d2f6e5cbf554e3c7be9a355b6975166a7923e
luizhlpaino <61785785+luizhlpaino@users.noreply.github.com>
2020-07-28 23:20:53 -0300

    adicionado script de plataforma

--- Assets/Scripts/PlayerPlatformScript.cs
+++ Assets/Scripts/PlayerPlatformScript.cs
@@ -8,14 +8,19 @@ public class PlayerPlatformScript : MonoBehaviour
     private Animator animator;
     private SpriteRenderer sprite;
 
+    [Header("Movement Variables")]
     public float m_Speed = 5f;
+    public float m_JumpForce = 10f;
+
 
     [Header("Animator Variables")]
     public string m_AnimatorAxisXName;
     public string m_AnimatorAxisYName;
-
+    
     private float m_AxisX;
     private float m_AxisY;
+    private bool m_IsJumping = false;
+    private bool m_IsGrounded = true;
 
     private void Start() {
         animator = GetComponent<Animator>();
@@ -23,16 +28,34 @@ public class PlayerPlatformScript : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>();    
     }
 
-    private void Update() {
-        m_AxisX = Input.GetAxis("Horizontal");
-
-        if(m_AxisX < .5f)
-            sprite.flipX();
-
-        m_AxisY = Input.GetAxis("Vertical");        
-    }
-
     private void FixedUpdate() {
-        rigidbody.velocity = new Vector2(m_AxisX * m_Speed, m_AxisY);
+
+        m_AxisX = Input.GetAxis("Horizontal");
+        m_AxisY = Input.GetAxis("Vertical");       
+
+        animator.SetFloat(m_AnimatorAxisXName, m_AxisX);
+        animator.SetFloat(m_AnimatorAxisYName, m_AxisY);
+
+        if(m_AxisX < -0.2f){
+            sprite.flipX = true;
+        } 
+        else if(m_AxisX > 0.2f) {
+            sprite.flipX = false;
+        }
+
+        if(m_IsGrounded && Input.GetButtonDown("Jump"))
+            m_IsJumping = true;                     
+        else
+            m_IsJumping = false;        
+
+        animator.SetBool("isJumping", m_IsJumping);
+        animator.SetFloat(m_AnimatorAxisXName, m_AxisX);        
+
+        rigidbody.velocity = new Vector2(m_AxisX * m_Speed, rigidbody.velocity.y);
+
+        if(m_IsJumping)
+             rigidbody.velocity = Vector2.up * m_JumpForce;
+             
+        
     }
 }
