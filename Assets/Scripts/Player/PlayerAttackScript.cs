e283c4bf67cc79e0fe289e614fcf76b711cdf644
luizhlpaino <61785785+luizhlpaino@users.noreply.github.com>
2020-08-31 00:52:29 -0300

    added attack example

---
+++ Assets/Scripts/Player/PlayerAttackScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour
+{
+    public float HitTime = 0.5f;
+    public float m_radius = 1f;
+    public LayerMask m_LayerMask;
+    protected CircleCollider2D m_collider;
+    protected Collider2D[] m_CollidedWith;
+    protected ContactFilter2D contactFilter;
+    
+    void OnEnable(){               
+        m_collider = GetComponent<CircleCollider2D>();                
+        Vector2 _position = new Vector2(transform.position.x, transform.position.y);
+        m_CollidedWith = Physics2D.OverlapCircleAll(_position, m_radius, m_LayerMask);
+        int _hitCount = m_CollidedWith.Length;
+        Debug.Log("HitCount: " + _hitCount);
+        for(int i = 0; i < _hitCount; i++){
+            Debug.Log(m_CollidedWith[i].gameObject.name);
+        }
+        StartCoroutine("HitWaitTime");        
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = new Color(255, 0, 0, 1);
+        Gizmos.DrawSphere(transform.position, m_radius);    
+    }
+
+    IEnumerator HitWaitTime(){        
+       yield return new WaitForSeconds(HitTime);
+       gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/Player/PlayerPlatformScript.cs
+++ Assets/Scripts/Player/PlayerPlatformScript.cs
@@ -4,29 +4,28 @@ using UnityEngine;
 
 public class PlayerPlatformScript : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private Animator animator;
-    private SpriteRenderer sprite;
+    [Header("Components")]
+        private Rigidbody2D rb;
+        private Animator animator;
+        private SpriteRenderer sprite;
 
     [Header("Sensors")]
-    public Transform groundCheck;
-    public float checkRadius;
-    public LayerMask whatIsGround;
-    public bool m_IsGrounded = false;
+        public Transform groundCheck;
+        public float checkRadius;
+        public LayerMask whatIsGround;
+        public GameObject hit;
+        public bool m_IsGrounded = false;
 
     [Header("Movement Variables")]
-    public float m_Speed = 5f;
-    public float m_JumpForce = 10f;
+        public float m_Speed = 5f;
+        public float m_JumpForce = 10f;
 
     [Header("Animator Variables")]
-    public string m_AnimatorAxisXName;
-    public string m_AnimatorAxisYName;
-
-    private float m_AxisX;
-    private float m_AxisY;
+        public string m_AnimatorAxisXName;
+        public string m_AnimatorAxisYName;
 
     [Header("State Variables")]        
-    public bool m_IsFacingRight = true;
+        public bool m_IsFacingRight = true; 
 
     private void Start() {
         animator = GetComponent<Animator>();
@@ -37,29 +36,33 @@ public class PlayerPlatformScript : MonoBehaviour
     void FixedUpdate() {
         m_IsGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
 
-        m_AxisX = Input.GetAxis("Horizontal");
-        m_AxisY = rb.velocity.y;       
-        Debug.Log(m_AxisY);
+        if(Input.GetButton("Action"))
+            hit.SetActive(true);
 
-        animator.SetFloat(m_AnimatorAxisXName, m_AxisX);        
-        animator.SetFloat(m_AnimatorAxisYName, m_AxisY);
+        float _AxisX = Input.GetAxis("Horizontal");
+        float _AxisY = rb.velocity.y;               
 
-        if( m_AxisX > 0.2f && !m_IsFacingRight || m_AxisX < -0.2f && m_IsFacingRight)
-            Flip();                                                        
+        if(m_AnimatorAxisXName != "")
+            animator.SetFloat(m_AnimatorAxisXName, _AxisX);        
+        
+        if(m_AnimatorAxisYName != "")
+            animator.SetFloat(m_AnimatorAxisYName, _AxisY);
 
-        animator.SetFloat(m_AnimatorAxisXName, m_AxisX);        
-        rb.velocity = new Vector2(m_AxisX * m_Speed, rb.velocity.y);                                     
+        if( _AxisX > 0f && !m_IsFacingRight || _AxisX < -0f && m_IsFacingRight){
+            m_IsFacingRight = !m_IsFacingRight;               
+        }
+
+        sprite.flipX = !m_IsFacingRight;
+        rb.velocity = new Vector2(_AxisX * m_Speed, rb.velocity.y);                                     
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = new Color(255, 0, 0, 1);
+        Gizmos.DrawSphere(groundCheck.position, checkRadius);    
     }
 
     void Update(){
         if(m_IsGrounded && Input.GetButtonDown("Jump"))
            rb.velocity = Vector2.up * m_JumpForce;
-    }
-
-    private void Flip(){
-        m_IsFacingRight = !m_IsFacingRight;
-        Vector3 Scaler = transform.localScale;
-        Scaler.x *= -1;
-        transform.localScale = Scaler;
     }
 }
