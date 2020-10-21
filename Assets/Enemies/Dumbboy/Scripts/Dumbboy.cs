60121b8844414e51c31e80a5c7ad68f85190d98d
Luiz Henrique <luiz.areacom@outlook.com>
2020-10-21 14:00:52 -0300

    fixed enemy script

--- Assets/Enemies/Dumbboy/Scripts/Dumbboy.cs
+++ Assets/Enemies/Dumbboy/Scripts/Dumbboy.cs
@@ -2,18 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cavewood.Models;
-public class Dumbboy : MonoBehaviour, IEnemy
+public class Dumbboy : Enemy
 {   
 #region Variables
-    [Header("Combat Variables")]          
-        public float m_Life = 20f;
-        public int m_Damage = 100;        
-        public float m_MaxAttackCountdown = 1.0f;
-        private float m_AttackCountdown = 0.0f;  
-        public float m_MaxStaggerTime = 0.5f;
-        private float m_StaggerCountdown = 0.0f;  
-        public float m_Speed = 3.0f; 
-        public float m_KnockbackForce = 5.0f;
+    [Header("Combat Variables")]                          
         public LayerMask m_PlayerLayerMask;
         private List<Collider2D> m_CollidedWith = new List<Collider2D>();        
     
@@ -21,103 +13,27 @@ public class Dumbboy : MonoBehaviour, IEnemy
         private GameObject m_Player;            
         private Animator m_SpriteAnimator;   
         private Collider2D m_AttackRange;        
-        private Rigidbody2D rb2d;            
-#endregion
-    IEnumerator Knocked() {
-        do {
-            m_StaggerCountdown += Time.deltaTime;
-            if(m_StaggerCountdown > m_MaxStaggerTime)
-                m_AttackCountdown = m_MaxStaggerTime;                
-            yield return null;
-        } while (m_StaggerCountdown < m_MaxStaggerTime);
-        m_StaggerCountdown = 0.0f;
+        private Rigidbody2D rb2d;
+
+    protected override void Attack()
+    {
+        throw new System.NotImplementedException();
     }
 
-    IEnumerator Attacking() {
-        do {                        
-            m_AttackCountdown += Time.deltaTime;
-            if(m_AttackCountdown > m_MaxAttackCountdown)
-                m_AttackCountdown = m_MaxAttackCountdown;                
-            yield return null;
-        } while(m_AttackCountdown < m_MaxAttackCountdown);
-        m_AttackCountdown = 0.0f;
-
-        ContactFilter2D _Filter = new ContactFilter2D();
-        List<Collider2D> _CollidedWith = new List<Collider2D>();        
-        _Filter.SetLayerMask(m_PlayerLayerMask);        
-        int _PlayerWasDamaged = m_AttackRange.OverlapCollider(_Filter, _CollidedWith);                     
-        if(_PlayerWasDamaged > 0){
-            m_Player.GetComponent<PlayerPlatformScript>().TakeDamage(
-                m_Damage,
-                5.0f,
-                transform.position.x
-            );
-        }   
+    protected override void Move()
+    {
+        throw new System.NotImplementedException();
     }
-
-    public void Attack() {                
-        rb2d.velocity.Set(0.0f, rb2d.velocity.y);
-        m_SpriteAnimator.SetTrigger("DidAttack");
-        StartCoroutine("Attacking");                  
-    }
-
-    public void Die() {
-        Destroy(transform.parent.gameObject);
-    }
-
-    public void Knockback(){    
-        StartCoroutine("Knocked");             
-        rb2d.velocity = new Vector2(
-            GetDirection(transform.position, m_Player.transform.position), 
-            0.5f
-        ) * m_KnockbackForce;
-    }
-
-    public void TakeDamage(float value) {
-        Knockback();
-        m_SpriteAnimator.SetTrigger("WasDamaged");
-        m_Life -= value;
-        if(m_Life <= 0)
-            Die();
-    }
+    #endregion
 
     private void Awake() {
         rb2d = GetComponent<Rigidbody2D>();
         m_SpriteAnimator = transform.Find("Sprite").gameObject.GetComponent<Animator>();
         m_AttackRange = transform.Find("AttackRange").gameObject.GetComponent<Collider2D>();
-        m_Player = GameObject.Find("Player");                               
+        m_Player = GameObject.FindGameObjectWithTag("Player");                               
     }    
 
-    private void FixedUpdate() {  
-        ContactFilter2D _Filter = new ContactFilter2D();
-        _Filter.SetLayerMask(m_PlayerLayerMask);             
-        int _CollidedCount = m_AttackRange.OverlapCollider(_Filter, m_CollidedWith);  
-        bool _CanAttack = (m_AttackCountdown == 0.0f && _CollidedCount > 0);    
-        bool _CanMove = (m_StaggerCountdown == 0.0f);   
-
-       // if(_CanAttack)
-            //Attack();    
-        //else if(_CanMove)        
-        if(_CanMove && !m_Player.GetComponent<PlayerPlatformScript>().IsInvulnerable())       
-            Move(m_Player.transform);
-    }
-
-    public float GetDirection(Vector3 PositionA, Vector3 PositionB){
-        float _Direction = 0.0f;
-
-        if((PositionA - PositionB).normalized.x > 0)
-            _Direction = 1.0f;
-        else 
-            _Direction = -1.0f;
-               
-        return _Direction;
-    }
-
-    public void Move(Transform Target)
-    {        
-        rb2d.velocity = new Vector2(
-            GetDirection(Target.position, transform.position) * m_Speed, 
-            rb2d.velocity.y
-        );
+    private void FixedUpdate() {                  
+        Move();
     }
 }
--- Assets/Scripts/Models/Enemy.cs
+++ Assets/Scripts/Models/Enemy.cs
@@ -5,12 +5,13 @@ namespace Cavewood.Models
 {
     public abstract class Enemy : MonoBehaviour 
     {
-        public int m_Health;
+        public int m_Life;
         public int m_Damage;     
-        private float m_StaggerCountdown = 0.0f;
         public float m_MaxStaggerTime = 0.3f;
-                               
+        private float m_StaggerCountdown = 0.0f;                
+
         protected abstract void Move();
+
         protected abstract void Attack();
 
         IEnumerator Knocked() {
@@ -21,8 +22,13 @@ namespace Cavewood.Models
             m_StaggerCountdown = 0.0f;
         }
 
-        public void TakeDamage(){
-            StartCoroutine("Knockback");
+        public void TakeDamage(int Damage){
+            m_Life -= Damage;
+
+            if(m_Life <= 0)
+                Die();
+            else
+                StartCoroutine("Knocked");            
         }       
 
         public void Die(){
