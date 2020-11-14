b85da0e291fe88777862a8537093169788f9fad5
ViniMolina <vini.molina2013@gmail.com>
2020-11-14 01:54:48 -0300

    Adicionado controle de inventario e ajustado a nuvem

---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour
+{
+    public GameObject cloudPrefab;
+    public float delay;
+    public static bool spawnClouds = true;
+
+    void Start()
+    {
+        Instantiate(cloudPrefab);
+    }
+}
