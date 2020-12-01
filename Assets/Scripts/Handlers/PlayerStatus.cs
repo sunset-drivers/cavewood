39a64a10bea8d0f6d0063c7013b0990da21c111b
luizhlpaino <61785785+luizhlpaino@users.noreply.github.com>
2020-12-01 01:27:31 -0300

    added heart containers

--- Assets/Scripts/Handlers/PlayerStatus.cs
+++ Assets/Scripts/Handlers/PlayerStatus.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class PlayerStatus : MonoBehaviour
 {
     [Header("Morale Info")]
+    public Sprite m_FullHeartSprite;
+    public Sprite m_HalfHeartSprite;
+    public Sprite m_EmptyHeartSprite;
+    public GameObject[] m_MoraleContainers;
     public GameObject m_MoraleBackground;
     public Text m_MoraleText;
 
@@ -19,7 +23,9 @@ public class PlayerStatus : MonoBehaviour
         m_MoneyText = m_MoneyBackground.transform.Find("txtMoney").GetComponent<Text>();
 
         m_MoraleBackground = transform.Find("bgMorale").gameObject;
-        m_MoraleText = m_MoraleBackground.transform.Find("txtMorale").GetComponent<Text>();        
+        m_MoraleText = m_MoraleBackground.transform.Find("txtMorale").GetComponent<Text>();
+
+        m_MoraleContainers = GameObject.FindGameObjectsWithTag("MoraleContainer");
     }
     
     void Update()
@@ -28,8 +34,25 @@ public class PlayerStatus : MonoBehaviour
             Debug.LogError("O GameManager não foi encontrado, adicione-o a cena.");
             return;
         } else {
-            m_MoraleText.text = StateManager.Instance.m_Morale.ToString();  
-            m_MoneyText.text = StateManager.Instance.m_Money.ToString();  
+            FillHearts();
+            //m_MoraleText.text = StateManager.Instance.m_Morale.ToString();  
+            //m_MoneyText.text = StateManager.Instance.m_Money.ToString();  
         }                    
+    }
+
+    private void FillHearts()
+    {
+        float m_Morale = StateManager.Instance.m_Morale/100f;
+        foreach(GameObject container in m_MoraleContainers)
+        {
+            if (m_Morale <= 0.0f)
+                container.GetComponent<Image>().sprite = m_EmptyHeartSprite;
+            else if (m_Morale > 0.0f && m_Morale < 1.0f)
+                container.GetComponent<Image>().sprite = m_HalfHeartSprite;
+            else
+                container.GetComponent<Image>().sprite = m_FullHeartSprite;
+
+            m_Morale -= 1;
+        }
     }
 }
